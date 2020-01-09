ee9867ea1638d435c01ea09f5b96289a3384b874
mdimai666 <mdimai666@mail.ru>
2020-01-09 10:07:54 +0900

    fix pm2 reduce size

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM node:12.14
+FROM node:12.14-alpine
 
 LABEL maintainer="mdimai666@mail.ru"
 
 WORKDIR /var/www/node1
 COPY package.json .
-RUN npm i
+RUN npm i && npm i -g pm2
 COPY . .
 
 EXPOSE 3002
 
-CMD [ "npm", "run", "pm2" ]   
+CMD [ "pm2-runtime", "bin/www" ]   
--- docker-run.ps1
+++ docker-run.ps1
@@ -1,7 +1,7 @@
 $host.UI.RawUI.BackgroundColor = "Blue"
 $host.UI.RawUI.ForegroundColor = "Red"
 Write-Output 'RUN CONTAINER node1'
-docker rm node2 -f
+# docker rm node2 -f
 docker run --rm -d -p 3002:3002/tcp --name node2 mdimai666/node2:latest
 [Console]::ResetColor()
 Write-Output ' '
